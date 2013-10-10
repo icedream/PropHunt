f940059259db50ec06fd8ac4629a9064e1567dcb
icedream <icedream2k9@die-optimisten.net>
2013-10-10 16:39:39 +0200

    Adding GMA stuff.

---
+++ compile.bat
@@ -0,0 +1,6 @@
+@echo off
+path %path%;%programfiles(x86)%\Steam\SteamApps\common\GarrysMod\bin
+mkdir tmp
+robocopy . tmp *.json *.lua *.wav *.mp3 *.jpg *.png *.txt /E /XD tmp /XF LICENSE.txt
+gmad create -folder "tmp" -out "prophunt.gma"
+pause
