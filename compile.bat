5f5ecda16e0ee2611087f882279a7bbe5ccddb87
icedream <icedream2k9@die-optimisten.net>
2013-12-18 10:54:49 +0100

    Fixing GMA stuff

--- compile.bat
+++ compile.bat
@@ -3,4 +3,5 @@ path %path%;%programfiles(x86)%\Steam\SteamApps\common\GarrysMod\bin
 mkdir tmp
 robocopy . tmp *.json *.lua *.wav *.mp3 *.jpg *.png *.txt /E /XD tmp /XF LICENSE.txt
 gmad create -folder "tmp" -out "prophunt.gma"
+rmdir /q /s tmp
 pause
