bf5ff8af5516d9456734cd930356cbf47fe14cca
icedream <icedream2k9@die-optimisten.net>
2013-12-24 11:19:57 +0100

    More debugging.

--- gamemodes/ph/gamemode/server/download.lua
+++ gamemodes/ph/gamemode/server/download.lua
@@ -1,13 +1,15 @@
 local function AddCSLuaFolder(fol)
-	LogF("AddCSLuaFolder", "Appending folder ", fol)
 	fol = string.lower(fol)
 	local _, folders = file.Find(fol .. "*", "LUA")
 	for _, folder in SortedPairs(folders, true) do
 		if folder ~= ".." then
+			LogF("AddCSLuaFolder", "Appending folder ", fol .. folder .. "/")
 			for _, File in SortedPairs(file.Find(fol .. folder .."/*.lua", "LUA")) do
 				LogF("AddCSLuaFolder", "Appending file ", fol .. folder .. "/" .. File)
 				AddCSLuaFile(fol .. folder .. "/" ..File)
 			end
+		else
+			LogF("AddCSLuaFolder", "Not appending folder ", fol .. folder .. "/")
 		end
 	end
 end
