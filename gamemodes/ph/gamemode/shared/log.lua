130b833306664b1187567b0d17355b839a182a8a
icedream <icedream2k9@die-optimisten.net>
2013-12-24 11:14:55 +0100

    Too lazy to fix this properly, switch to Msg/MsgN.

--- gamemodes/ph/gamemode/shared/log.lua
+++ gamemodes/ph/gamemode/shared/log.lua
@@ -1,26 +1,9 @@
 function Log(...)
-	printResult = "[PH] "
-	for _, v in ipairs({...}) do
-		if IsValid(v) && !!v["Name"] then
-			printResult = printResult .. v:Name()
-		else
-			printResult = printResult .. tostring(v)
-		end
-		printResult = printResult .. " "
-	end
-	print(printResult)
+	Msg("[PH] ")
+	MsgN({...})
 end
 
 function LogF(fn, ...)
-	if !fn then fn = "<unknown>" end
-	printResult = "[PH] " .. fn .. ":"
-	for _, v in ipairs({...}) do
-		if IsValid(v) && !!v["Name"] then
-			printResult = printResult .. v:Name()
-		else
-			printResult = printResult .. tostring(v)
-		end
-		printResult = printResult .. " "
-	end
-	print(printResult)
+	Msg("[PH] " .. (fn or "<unknown>") .. ": ")
+	MsgN({...})
 end
